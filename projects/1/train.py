96606cded12f227735b259ce508238c4d71e7fb0
Ledio Deda <33701673+lediau@users.noreply.github.com>
2023-03-19 02:21:47 +0300

    Create train.py

---
+++ projects/1/train.py
@@ -0,0 +1,46 @@
+import os, sys
+import logging
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from joblib import dump
+
+from model import model, fields
+
+
+logging.basicConfig(level=logging.DEBUG)
+logging.info("CURRENT_DIR {}".format(os.getcwd()))
+logging.info("SCRIPT CALLED AS {}".format(sys.argv[0]))
+logging.info("ARGS {}".format(sys.argv[1:]))
+
+
+try:
+  proj_id = sys.argv[1] 
+  train_path = sys.argv[2]
+except:
+  logging.critical("Need to pass both project_id and train dataset path")
+  sys.exit(1)
+
+
+logging.info(f"TRAIN_ID {proj_id}")
+logging.info(f"TRAIN_PATH {train_path}")
+
+
+read_table_opts = dict(sep="\t", names=fields, index_col=False)
+df = pd.read_table(train_path, **read_table_opts)
+
+df_x, df_y = df.iloc[:, 2:], df.iloc[:, 1]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    df_x, df_y, test_size=0.33, random_state=42
+)
+
+model.fit(X_train, y_train)
+
+model_score = model.score(X_test, y_test)
+
+logging.info(f"model score: {model_score:.3f}")
+
+logging.info("model fitted!")
+dump(model, "{}.joblib".format(proj_id))
+logging.info("dumped!")
