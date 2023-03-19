47b41fcb20abaa55ce68709a17105c3da94f339a
Ledio Deda <33701673+lediau@users.noreply.github.com>
2023-03-19 06:15:03 +0300

    Update filter.sh

--- projects/1/filter.sh
+++ projects/1/filter.sh
@@ -1,9 +1,11 @@
-HADOOP_CUR=/usr/hdp/current/
-HADOOP_EXE=$HADOOP_CUR/hadoop-client/bin/hadoop
-HADOOP_STREAM_JAR=$HADOOP_CUR/hadoop-mapreduce-client/hadoop-streaming.jar
+#!/bin/bash
+
+HADOOP_EXE=/usr/bin/yarn
+HADOOP_STREAM_JAR=/usr/lib/hadoop-mapreduce/hadoop-streaming.jar
+
 FILES=$1
 INPUT=$2
 OUTPUT=$3
 MAPPER=$4
-
-$HADOOP_EXE jar $HADOOP_STREAM_JAR  -files $FILES -D mapred.reduce.tasks=0 -input $INPUT -output $OUTPUT -mapper "$MAPPER"
+REDUCER=$5
+$HADOOP_EXE jar $HADOOP_STREAM_JAR  -files $FILES -input $INPUT -output $OUTPUT -mapper "$MAPPER" -reducer "$REDUCER"
